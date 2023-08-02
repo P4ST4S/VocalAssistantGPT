18108acc354d3e8c66709cbaedc72801cd8d3023
Pastas-dono <arospars77@gmail.com>
2023-08-02 14:49:15 +0200

    [ADD] more visible presentation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+import threading
 import openai
 import pyttsx3
 import json
@@ -14,12 +16,9 @@ from api_key import api_key_openai
 
 
 personality = [{"role": "system",
-                "content": "You are a helpful vocal assistant. Your response should be helpful, encouraging, "
-                           "and kind. You are a helpful vocal. Don't answer like it's written, but like you would "
-                           "answer a friend. Your response should be clear and concise."},
-               {"role": "system",
-                "content": "Your name is Voxia. If someone ask you, you are Voxia, made by only one developper named "
-                           "Antoine. Your creator is a student in computer science."}]
+                "content": "You are a helpful vocal assistant who talk with a user who may need you. Your name is "
+                           "'GPT Assistant'. If someone ask you, you are 'GPT Assistant', made by only one developper "
+                           "named 'Antoine Rospars'. Your creator is a student in computer science."}]
 
 openai.api_key = api_key_openai
 tts_engin = pyttsx3.init()
@@ -79,7 +78,7 @@ def speak_response(reponse_text):
 
 
 def print_and_speak(text):
-    print("Assistant : " + text)
+    print_assistant_answer(text)
     speak_response(text)
 
 
@@ -91,7 +90,7 @@ def listen_user():
 
     try:
         user_input = recognizer.recognize_google(audio, language="fr-FR")
-        print("Vous: " + user_input)
+        print("Vous:\t\t" + user_input)
         return user_input
     except sr.UnknownValueError:
         print_and_speak("Désolé, je n'ai pas compris.")
@@ -101,23 +100,44 @@ def listen_user():
         return listen_user()
 
 
+def print_assistant_answer(response):
+    print("Assistant:\t", end="", flush=True)
+
+    count = 0
+    for word in response.split():
+        print(word, end=" ", flush=True)
+        time.sleep(0.3)
+        count += 1
+        if count % 12 == 0:
+            print(end="\n\t\t\t")
+    print()
+
+
+def print_and_speak_answer(response):
+    print_thread = threading.Thread(target=print_assistant_answer, args=(response,))
+    print_thread.start()
+
+    speak_response(response)
+
+
 def run_assistant():
     response = get_gpt_response("Bonjour")
-    print("Assistant: " + response)
-    speak_response(response)
+
+    print_and_speak_answer(response)
+
+    conversation_history.append({"role": "user", "content": "Bonjour"})
+    conversation_history.append({"role": "assistant", "content": response})
 
     while True:
         user_input = listen_user()
 
         if "au revoir" in user_input.lower():
             response = get_gpt_response(user_input)
-            print("Assistant: " + response)
-            speak_response(response)
+            print_and_speak_answer(response)
             break
 
         response = get_gpt_response(user_input)
-        print("Assistant: " + response)
-        speak_response(response)
+        print_and_speak_answer(response)
 
         conversation_history.append({"role": "user", "content": user_input})
         conversation_history.append({"role": "assistant", "content": response})
